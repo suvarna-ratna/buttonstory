59a5b8040c4ac98764d10516c8e828ea1c71dee2
suvarna <suvarna.akkala@gmail.com>
2022-01-21 13:24:48 +0100

    color

--- src/Components/CounterComponent/CounterComponent.js
+++ src/Components/CounterComponent/CounterComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./CounterComponent.styles.css";
 
-export default function CounterComponent() {
+export default function CounterComponent(backgroundColor) {
 let [countNum, setCount] = React.useState(0);
 let increaseValaue = () => {
 setCount((countNum += 1)); //increases the count by 1
@@ -20,17 +20,17 @@ setCount(0); //resets the count to 0
   return (
     <div className="counter-component">
       <h1> {countNum} </h1>
-<button onClick={increaseValaue}>+</button> 
-<button onClick={decreaseValue}>-</button>
-<button onClick={resetValue}>Reset</button>
+<button onClick={increaseValaue} style={backgroundColor}>+</button> 
+<button onClick={decreaseValue} style={backgroundColor}>-</button>
+<button onClick={resetValue} style={backgroundColor}>Reset</button>
     </div>
   );
 }
 
 CounterComponent.propTypes = {
- 
+  backgroundColor: PropTypes.string,
 };
 
 CounterComponent.defaultProps = {
-  
+  backgroundColor: 'aqua'
 };
--- src/Components/CounterComponent/CounterComponent.stories.js
+++ src/Components/CounterComponent/CounterComponent.stories.js
@@ -6,9 +6,20 @@ export default {
   title: "Components / Counter Component",
   component: CounterComponent,
   argTypes: {
+    backgroundColor: { control: 'color' },
     onClick: { action: "clicked" },
   },
   parameters: { actions: { argTypesRegex: "^on.*" } },
 };
 
-export const Component = () => <CounterComponent />;
+export const Component = (args) => <CounterComponent {...args}/>;
+Component.args={
+  backgroundColor: 'green'
+}
+
+export const Component2 = (args) => <CounterComponent {...args}/>;
+Component2.args={
+  backgroundColor: 'blue'
+}
+
+export const Component3 = (args) => <CounterComponent {...args}/>;
