cb7954f8e59a92d7334fb233908a0c209734e8c0
suvarna <suvarna.akkala@gmail.com>
2022-01-21 12:56:46 +0100

    knobs error

--- src/Components/CounterComponent/CounterComponent.stories.js
+++ src/Components/CounterComponent/CounterComponent.stories.js
@@ -1,19 +1,14 @@
-import { text } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
-import React from 'react';
+import React from "react";
 
-import CounterComponent from './CounterComponent';
+import CounterComponent from "./CounterComponent";
 
 export default {
-  title: 'Components / Counter Component',
+  title: "Components / Counter Component",
   component: CounterComponent,
   argTypes: {
-    onClick: { action: 'clicked' },
+    onClick: { action: "clicked" },
   },
-  parameters: { actions: { argTypesRegex: '^on.*' } },
+  parameters: { actions: { argTypesRegex: "^on.*" } },
 };
 
-export const Component = () => (
-  <CounterComponent
-  />
-);
+export const Component = () => <CounterComponent />;
